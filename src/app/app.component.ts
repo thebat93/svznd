aab0c1cdb034d49d62a175d5b7973de61c907da3
Fedor Pesyak <femm@mail.cam>
2017-10-20 16:31:21 +0300

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <my-map></my-map>
+  <layers-list></layers-list>
+  `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import "leaflet";
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map.component';
+import { LayersListComponent } from './layers-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    LayersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
